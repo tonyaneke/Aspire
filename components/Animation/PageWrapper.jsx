2eb6912153da823b4bd9b673491de4cfbb1678ac
user <77333061+tonyaneke@users.noreply.github.com>
2024-04-02 18:54:12 +0100

    animated ui1.0

--- components/Animation/PageWrapper.jsx
+++ components/Animation/PageWrapper.jsx
@@ -9,7 +9,7 @@ export default function PageWrapper({ children }) {
           initial={{ opacity: 0, x: 25 }}
           animate={{ opacity: 1, x: 0 }}
           exit={{ opacity: 0, x: 25 }}
-          transition={{ delay: 1 }}
+          transition={{ delay: 0.5 }}
         >
           {children}
         </motion.div>
